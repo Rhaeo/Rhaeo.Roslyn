5b679b2553e8102a1dabd9de45c47e665ba05b8a
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-10 20:39:33 +0200

    Add a demonstration console project
    This should be useful for tweaking and testing ideas before releasing
    the Nuget package.

---
+++ Sources/Rhaeo.Roslyn.Demonstrations/Program.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Rhaeo.org">
+//   Licenced under the MIT license.
+// </copyright>
+// <summary>
+//   Defines the entry point of the application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rhaeo.Roslyn.Demonstrations
+{
+  /// <summary>
+  /// Defines the entry point of the application.
+  /// </summary>
+  public static class Program
+  {
+    #region Methods
+
+    /// <summary>
+    /// The application entry-point.
+    /// </summary>
+    private static void Main()
+    {
+    }
+
+    #endregion
+  }
+}
